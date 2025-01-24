041db31b4a4af0b637e6e14469456dfdedf96659
Arina Medvedeva <akuchuruk@gmail.com>
2025-01-24 18:47:09 -0500

    removed preprocessing steps from ll function

--- src/poisson_point_process/monte_carlo_approx.py
+++ src/poisson_point_process/monte_carlo_approx.py
@@ -4,7 +4,7 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 
-from .utils import slice_array
+from .utils import slice_array, reshape_for_vmap, compute_max_window_and_adjust
 
 
 @partial(jax.jit, static_argnums=(2,))
@@ -19,47 +19,39 @@ def linear_non_linear(dts, weights, bias, basis_fn, inverse_link=jax.nn.softplus
     ll = inverse_link(sum_basis_and_dot(weights, dts, basis_fn) + bias)
     return ll
 
-
-def compute_unnormalized_log_likelihood(
-    weights,
-    bias,
+def compute_summed_ll(
     spike_times_concat,
     spike_id_concat,
-    neu_spk_idx,
-    history_window_size,
+    shifted_idx,
+    n_batches_scan,
     max_window_size,
+    params,
     basis_fn,
-    n_batches_scan=1,
     inverse_link=jax.nn.softplus,
+    log=True
 ):
-    max_window_size = int(max_window_size)
+    if log:
+        optional_log = jnp.log
+    else:
+        optional_log = lambda x: x
 
-    # define max window and adjust indices
-    delta_idx = jax.nn.relu(max_window_size - neu_spk_idx[0])
-    shifted_idx = neu_spk_idx + delta_idx
+    weights, bias = params
 
-    spike_times_concat = np.hstack(
-        (jnp.full(delta_idx, -history_window_size - 1), spike_times_concat)
-    )
-    spike_id_concat = np.hstack((jnp.full(delta_idx, 0), spike_id_concat))
-
-    shifted_idx = np.hstack(
-        (shifted_idx, jnp.full(-shifted_idx.size % n_batches_scan, delta_idx))
-    )
-    update_idx_array = shifted_idx.reshape(shifted_idx.size // n_batches_scan, -1)
+    shifted_idx_array, padding = reshape_for_vmap(shifted_idx, n_batches_scan)
 
     # body of the scan function
     def scan_fn(lam_s, i):
         dts = slice_array(
-            spike_times_concat, i, max_window_size
+            spike_times_concat, i+1, max_window_size+1
         ) - jax.lax.dynamic_slice(spike_times_concat, (i,), (1,))
-        idxs = slice_array(spike_id_concat, i, max_window_size)
-        ll = jnp.log(
+        idxs = slice_array(spike_id_concat, i+1, max_window_size+1)
+        ll = optional_log(
             linear_non_linear(dts, weights[idxs], bias, basis_fn, inverse_link)
         )
         lam_s += jnp.sum(ll)
         return jnp.sum(lam_s), None
 
     scan_vmap = jax.vmap(lambda idxs: jax.lax.scan(scan_fn, jnp.array(0), idxs))
-    out, _ = scan_vmap(update_idx_array)
-    return out, scan_vmap
+    out, _ = scan_vmap(shifted_idx_array)
+    sub, _ = scan_vmap(padding[:,None])
+    return jnp.sum(out) - jnp.sum(sub)
