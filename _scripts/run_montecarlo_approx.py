fixed dts computation error in scan

_scripts/run_montecarlo_approx.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 from poisson_point_process import simulate
 from poisson_point_process.utils import compute_max_window_size
-from poisson_point_process.poisson_process_glm import ContinuousGLM
+from poisson_point_process.poisson_process_glm import ContinuousMC
 
 jax.config.update("jax_enable_x64", True)
 os.environ["JAX_PLATFORM_NAME"] = "cpu"
@@ -18,20 +18,21 @@ os.environ["JAX_PLATFORM_NAME"] = "cpu"
 all_to_all = False
 
 # generate data
-n_neurons = 2
+n_neurons = 5
 target_neu_id = 0
 history_window = 0.3
 tot_time_sec = 1000
 binsize = 0.01
 window_size = int(history_window / binsize)
-n_basis_funcs = 2
+n_basis_funcs = 4
 n_bins_tot = int(tot_time_sec / binsize)
 n_batches_scan = 1
 inverse_link = jax.nn.softplus
 rc_basis = nmo.basis.RaisedCosineLogConv(n_basis_funcs, window_size=window_size)
-_, kernels = rc_basis.evaluate_on_grid(window_size)
+time, kernels = rc_basis.evaluate_on_grid(window_size)
+time *= history_window
 
-np.random.seed(216)
+np.random.seed(123)
 
 t0 = perf_counter()
 if all_to_all:
@@ -42,7 +43,7 @@ if all_to_all:
     biases = jnp.array(np.abs(np.random.normal(baseline_fr, baseline_fr/10, n_neurons))) + np.log(binsize)
     bias_true = biases[target_neu_id]
 
-    weights_true = jnp.array(np.random.normal(-0.1, 0.01, size=(n_neurons, n_neurons, n_basis_funcs)))
+    weights_true = jnp.array(np.random.normal(0, 0.1, size=(n_neurons, n_neurons, n_basis_funcs)))
     params = (weights_true, biases)
 
     spike_counts, _ = simulate.poisson_counts_recurrent(n_bins_tot, n_neurons, window_size, kernels, params, inverse_link)
@@ -77,12 +78,11 @@ else:
     posts_rate_per_sec = 2
     # rescaled proportionally to the binsize
     bias_true =  posts_rate_per_sec + np.log(binsize)
-    weights_true = np.random.normal(0, 1, n_neurons * n_basis_funcs)
-    X, y_counts, X_counts, lam_posts =  simulate.poisson_counts(pres_rate_per_sec, posts_rate_per_sec, binsize,
+    weights_true = np.random.normal(0, 0.3, n_neurons * n_basis_funcs)
+    # weights_true = jnp.array([-0.5, -0.7, 0.1, 0.9])
+    X, y_counts, X_counts, lam_posts = simulate.poisson_counts(pres_rate_per_sec, posts_rate_per_sec, binsize,
                                                n_bins_tot, n_neurons, weights_true, window_size, rc_basis, inverse_link)
     inv_rates = np.log(np.exp(lam_posts) - 1)
-
-    print(jnp.hstack((y_counts[:,None], X_counts)).shape)
 
     print(f"mean spikes per neuron: {jnp.mean(jnp.hstack((y_counts[:,None], X_counts)).sum(0))}")
     print(f"max spikes per bin: {jnp.hstack((y_counts[:,None], X_counts)).max()}")
@@ -114,58 +114,99 @@ obs_model_kwargs = {
     "max_window": max_window,
     "n_batches_scan": n_batches_scan,
     "mc_random_key": jax.random.PRNGKey(0),
+    "mc_n_samples": y_spikes.shape[1]*5
 }
+print(y_spikes.shape[1]*5)
 
 # model = ContinuousGLM(solver_name="LBFGS", regularizer="Ridge", regularizer_strength=1., obs_model_kwargs=obs_model_kwargs, solver_kwargs={"has_aux": True})
-model = ContinuousGLM(solver_name="GradientDescent", obs_model_kwargs=obs_model_kwargs, solver_kwargs={"has_aux": True, "stepsize": 1, "acceleration": False})
+# model = ContinuousGLM(solver_name="LBFGS", obs_model_kwargs=obs_model_kwargs, solver_kwargs={"has_aux": True})
+model = ContinuousMC(solver_name="GradientDescent", obs_model_kwargs=obs_model_kwargs, solver_kwargs={"has_aux": True, "acceleration": False, "stepsize": 0.0005})
 
-true_params = (jnp.concatenate((jnp.zeros(2),weights_true)), jnp.array([bias_true]))
+true_params = (jnp.concatenate((jnp.zeros(n_basis_funcs),weights_true)), jnp.array([bias_true]))
+# true_params = (weights_true, jnp.array([posts_rate_per_sec]))
+# true_params = (jnp.zeros_like(weights_true), jnp.array([bias_true]))
 params = model.initialize_params(X_spikes, y_spikes, init_params=true_params)
-print(params)
+print(weights_true)
 state = model.initialize_state(X_spikes, y_spikes, params)
 
-num_iter = 200
+num_iter = 1000
 tt0 = perf_counter()
 error = np.zeros(num_iter)
+error_sr = 10
 for step in range(num_iter):
     t0 = perf_counter()
     params, state = model.update(params, state, X_spikes, y_spikes)
-    # print(params)
-    error[step] = model._negative_log_likelihood(X_spikes, y_spikes, params, state.aux)
-    if step % 10 == 0:
-        print(f"step {step}, {perf_counter() - t0}")
+    t1 = perf_counter()
+    # error[step] = model._negative_log_likelihood(X_spikes, y_spikes, params, state.aux)
+    if step % error_sr == 0:
+        error[step] = model._negative_log_likelihood(X_spikes, y_spikes, params, state.aux)
+        print(f"step {step}, {t1 - t0}")
+        print(params)
 print(f"fitted model, {perf_counter() - tt0}")
+error = error[error!=0]
 
 obs_model = nmo.observation_models.PoissonObservations(inverse_link)
 model_exact = nmo.glm.GLM(solver_name="LBFGS",observation_model=obs_model).fit(X_discrete, y_discrete)
 
 weights_nemos, bias_nemos = model_exact.coef_.reshape(-1,n_basis_funcs)[1:], model_exact.intercept_
-weights_mc, bias_mc = model.coef_.reshape(-1,n_basis_funcs)[1:], model.intercept_
-filters_mc = np.dot(weights_mc, kernels.T)
-filters_nemos = np.dot(weights_nemos, kernels.T)
+weights_mc, bias_mc = model.coef_.reshape(-1,n_basis_funcs), model.intercept_
+filters_mc = np.dot(weights_mc[1:], kernels.T) + bias_mc
+filters_nemos = np.dot(weights_nemos, kernels.T) + bias_nemos
 if len(weights_true.shape) == 3:
     weights_true = weights_true[target_neu_id]
-filters_true = np.dot(weights_true.reshape(-1,n_basis_funcs), kernels.T)
+filters_true = np.dot(weights_true.reshape(-1,n_basis_funcs), kernels.T) + bias_true
 
 sc_f = np.max(np.abs(filters_mc)) / np.max(np.abs(filters_true))
-print(weights_true)
-print(weights_mc)
+print(f"true weights: {weights_true}")
+print(f"inferred weights: {model.coef_}")
 
-fig, axs = plt.subplots(1,2,figsize=(7,5))
-for n, ax in enumerate(axs.flat):
-    # ax.plot(jax.nn.softplus(filters_mc[n]), c='r', label='approx')
-    ax.plot(filters_true[n], c='g', label='true')
-    ax.plot(filters_nemos[n], c='k', label='exact')
-    # ax.plot(filters_mc[n], c='r', label='approx')
-    # ax.plot(filters_true[n], c='g', label='true')
-    # ax.plot(filters_nemos[n], c='k', label='exact')
-plt.legend()
+fig, axs = plt.subplots(3,2,figsize=(7,9))
+axs = axs.flat
+for n, ax in enumerate(axs):
+    if n==0:
+        ax.plot(np.arange(0, num_iter,error_sr),error, c='k')
+        ax.set_xlabel("epoch")
+        ax.set_ylabel("loss")
+    else:
+        line1 = ax.plot(time, jax.nn.softplus(filters_true[n-1]), c='g', label='true')
+        line2 = ax.plot(time, jax.nn.softplus(filters_nemos[n-1]), c='k', label='exact')
+        line3 = ax.plot(time, jax.nn.softplus(filters_mc[n-1]) * binsize, c='r', label='approx')
+        ax.set_xlabel("time from spike")
+        ax.set_ylabel("gain")
+        # ax1 = ax.twinx()
+        # line3 = ax1.plot(time, jax.nn.softplus(filters_mc[n-1])*binsize, c='r', label='approx')
+        # ax1.tick_params(axis='y', labelcolor='r')
 
-fig, axs = plt.subplots(1,2,figsize=(7,5))
-for n, ax in enumerate(axs.flat):
-    ax.plot(filters_mc[n], c='r', label='approx')
-    # ax.plot(jax.nn.softplus(filters_true[n]), c='g', label='true')
+        if n==len(axs)-1:
+            # ax1.set_ylabel("gain (approx)", color='r')
+            lines = [line1[0], line2[0], line3[0]]
+            labels = [l.get_label() for l in lines]
+            ax.legend(lines, labels, loc="upper right")
 
-plt.figure()
-plt.plot(error)
+plt.tight_layout()
 plt.show()
+
+
+# fig, axs = plt.subplots(1,2,figsize=(9,5))
+# for n, ax in enumerate(axs.flat):
+#     line1 = ax.plot(time, filters_true[n], c='g', label='true')
+#     line2 = ax.plot(time, filters_nemos[n], c='k', label='exact')
+#     ax.set_xlabel("time from spike")
+#     ax.set_ylabel("gain")
+#     ax2 = ax.twinx()
+#     line3 = ax2.plot(time, filters_mc[n], c='r', label='approx')
+#     if n==1:
+#         ax2.set_ylabel("gain (MC)", color='r')
+#         ax2.tick_params(axis='y', labelcolor='r')
+#         lines = [line1[0], line2[0], line3[0]]
+#         labels = [l.get_label() for l in lines]
+#         ax.legend(lines, labels, loc="upper right")
+#     else:
+#         ax2.tick_params(labelright=False)
+# plt.tight_layout()
+
+# plt.figure()
+# plt.plot(error, c='k')
+# plt.xlabel("epoch")
+# plt.ylabel("loss")
+# plt.show()

src/poisson_point_process/poisson_process_glm.py
@@ -21,7 +21,7 @@ from nemos.typing import DESIGN_INPUT_TYPE
 
 ModelParams = Tuple[jnp.ndarray, jnp.ndarray]
 
-class ContinuousGLM(BaseRegressor):
+class ContinuousMC(BaseRegressor):
     def __init__(
         self,
         obs_model_kwargs: dict,
@@ -44,13 +44,15 @@ class ContinuousGLM(BaseRegressor):
         self.scale_ = None
         self.dof_resid_ = None
 
-        # required to cmpute neg ll - no separate observation model class
+        # required to compute neg ll - no separate observation model class
         self.n_basis_funcs = obs_model_kwargs["n_basis_funcs"]
         self.n_batches_scan = obs_model_kwargs["n_batches_scan"]
         self.max_window = obs_model_kwargs["max_window"]
         self.history_window = obs_model_kwargs["history_window"]
-        self.random_key = obs_model_kwargs["mc_random_key"]
         self.inverse_link_function = obs_model_kwargs["inverse_link_function"]
+        self.random_key = obs_model_kwargs["mc_random_key"]
+        self.M = int(obs_model_kwargs["mc_n_samples"])
+        self.T = 1000
 
     @staticmethod
     def _check_params(
@@ -186,7 +188,7 @@ class ContinuousGLM(BaseRegressor):
         s_m = jax.random.choice(keys[0], valid_spikes, shape=(M,), replace=True)
         epsilon_m = jax.random.uniform(keys[1], shape=(M,), minval=0.0, maxval=self.history_window)
         tau_m = s_m + epsilon_m
-        tau_m_idx = jnp.searchsorted(X[0], tau_m, "right")
+        tau_m_idx = jnp.searchsorted(X[0], tau_m, "right")-1
         mc_spikes = jnp.vstack((tau_m, tau_m_idx))
 
         return mc_spikes
@@ -202,27 +204,29 @@ class ContinuousGLM(BaseRegressor):
 
         weights, bias = params
         weights = weights.reshape(-1, self.n_basis_funcs)
+        # weights = jnp.vstack((jnp.zeros(self.n_basis_funcs), weights))
 
-        shifted_idx_array, padding = reshape_for_vmap(y[1].astype(int), self.n_batches_scan)
+        shifted_spikes_array, padding = reshape_for_vmap(y, self.n_batches_scan)
 
         # body of the scan function
         def scan_fn(lam_s, i):
             spk_in_window = slice_array(
-                X, i + 1, self.max_window + 1
+                X, i[1].astype(int) + 1, self.max_window + 1
             )
 
-            dts = spk_in_window[0] - jax.lax.dynamic_slice(X, (0, i), (1, 1))
+            dts = spk_in_window[0] - i[0]
 
             ll = optional_log(
-                self.linear_non_linear(dts[0], weights[spk_in_window[1].astype(int)], bias)
+                self.linear_non_linear(dts, weights[spk_in_window[1].astype(int)], bias)
             )
+            # jax.debug.print("i: {}", i)
+            # jax.debug.print("ll: {}", ll)
             lam_s += jnp.sum(ll)
-            return jnp.sum(lam_s), None
+            return lam_s, None
 
         scan_vmap = jax.vmap(lambda idxs: jax.lax.scan(scan_fn, jnp.array(0), idxs))
-        out, _ = scan_vmap(shifted_idx_array)
-        sub, _ = scan_vmap(padding[:, None])
-        # return (jnp.sum(out) - jnp.sum(sub)) / y.shape[1]
+        out, _ = scan_vmap(shifted_spikes_array)
+        sub, _ = scan_vmap(padding[None,:])
         return jnp.sum(out) - jnp.sum(sub)
 
     def _negative_log_likelihood(
@@ -235,8 +239,7 @@ class ContinuousGLM(BaseRegressor):
             log=True
         )
 
-        M = y.shape[1]
-        mc_samples = self.draw_mc_sample(X, M, random_key)
+        mc_samples = self.draw_mc_sample(X, self.M, random_key)
         mc_estimate = self.compute_summed_ll(
             X,
             mc_samples,
@@ -244,9 +247,9 @@ class ContinuousGLM(BaseRegressor):
             log=False
         )
 
-        self.predicted_rate = mc_estimate / M
+        estimated_rate = (self.T/self.M) * mc_estimate
 
-        return self.predicted_rate - log_lam_y
+        return estimated_rate - log_lam_y
 
     def _predict_and_compute_loss(
         self,
@@ -371,6 +374,8 @@ class ContinuousGLM(BaseRegressor):
         #add max window padding to X and y
         X, y = adjust_indices_and_spike_times(X, y, self.history_window, self.max_window)
 
+        self.T = jnp.ceil(X[0, -1])
+
         init_params = self.initialize_params(X, y, init_params=init_params)
 
         self.initialize_state(X, y, init_params)
@@ -404,6 +409,8 @@ class ContinuousGLM(BaseRegressor):
 
         #add max window padding to X and y
         X, y = adjust_indices_and_spike_times(X, y, self.history_window, self.max_window)
+
+        self.T = int(jnp.ceil(X[0, -1]))
 
         # perform a one-step update
         opt_step = self._solver_update(params, opt_state, self.random_key, X, y, *args, **kwargs)

src/poisson_point_process/utils.py
@@ -29,11 +29,12 @@ def adjust_indices_and_spike_times(X, y, history_window, max_window):
     return shifted_X, shifted_y
 
 @partial(jax.jit, static_argnums=1)
-def reshape_for_vmap(spike_idx, n_batches_scan):
-    padding = jnp.full(-spike_idx.size % n_batches_scan, spike_idx[0])
-    shifted_idx = jnp.hstack(
-        (spike_idx, padding)
+def reshape_for_vmap(spikes, n_batches_scan):
+    padding_shape = (-spikes.shape[1] % n_batches_scan,)
+    padding = jnp.full((spikes.shape[0],) + padding_shape, spikes[:, :1])
+    shifted_spikes = jnp.hstack(
+        (spikes, padding)
     )
-    shifted_idx_array = shifted_idx.reshape(shifted_idx.size // n_batches_scan, -1)
+    shifted_spikes_array = shifted_spikes.reshape(n_batches_scan, spikes.shape[0], -1).transpose(0,2,1)
 
-    return shifted_idx_array, padding
+    return shifted_spikes_array, padding.transpose(1,0)