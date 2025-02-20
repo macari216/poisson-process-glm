810d2d728fd733da14eb8adecb197f7491612716
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2025-02-19 22:08:50 -0500

    start quite off

--- _scripts/exact_simulation_poisson_point_process.py
+++ _scripts/exact_simulation_poisson_point_process.py
@@ -76,9 +76,9 @@ jit_parametric_cumul = jax.jit(parametric_cumul)
 @jax.jit
 def neg_log_likelihood(params, window_size, post_synaptic, tmax):
     log_lam = jnp.log(jit_parametric_intensity(post_synaptic, params, window_size))
-    return  jit_parametric_cumul(tmax, params, window_size) - log_lam.sum()
+    return  jit_parametric_cumul(tmax, params, window_size) / log_lam.shape[0]  - log_lam.mean()
 
-p0 = jnp.asarray([0.05, 2])
+p0 = jnp.asarray([1, 2], dtype=float)
 pg = ProjectedGradient(fun=neg_log_likelihood, projection=projection.projection_box)
 out = pg.run(p0, (1E-6, jnp.inf), window_size=ws, post_synaptic=spike_times, tmax=T)
 
